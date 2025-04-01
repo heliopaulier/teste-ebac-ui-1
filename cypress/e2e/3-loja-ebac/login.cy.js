ade25a4149d30e328baaf615e7c5c6c6ac8cef3a
Helio Paulier <helio.paulier@gmail.com>
2025-04-01 08:28:42 -0300

    Teste Ebac

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,39 @@
+/// <reference types="cypress"/>
+
+describe('Funionlidade: Login', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        cy.screenshot
+    });
+
+    it('1 - deve fazer login com sucesso', () => {        
+        cy.get('#username').type('paulier.helio@gmail.com')
+        cy.get('#password').type('Heli0@19')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, paulier.helio (não é paulier.helio? Sair)')       
+    })
+
+    it('2 - Deve exibir uma mensagem de erro ao inserir um user invalido', () => {
+        cy.get('#username').type('teste@gmail.com')
+        cy.get('#password').type('Heli0@19')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('exist')       
+    });
+
+
+    it('3 - Deve exibir uma mensagem de erro ao exibir uma senha ivalida', () => {
+        cy.get('#username').type('paulier.helio@gmail.com')
+        cy.get('#password').type('123456')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('exist') 
+        
+    });
+})
+
+
+
+
---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('1 - deve selecionar um produto da lista', () => {
+        //cy.get('.product-block ').eq(2).click()
+        cy.get('.product-block ').contains('Aero Daily Fitness Tee').click()
+        cy.get('.woocommerce-product-details__short-description > p').should('exist')
+    });
+    
+    
+});
